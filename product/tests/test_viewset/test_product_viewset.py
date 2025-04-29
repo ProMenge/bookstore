008b8993529820c7e3a6167bc31fea3c6cb71a9e
Fred Menge <mengeprofissional@gmail.com>
2025-04-29 16:04:45 -0300

    :test_tube: test: creating a delete product test

--- product/tests/test_viewset/test_product_viewset.py
+++ product/tests/test_viewset/test_product_viewset.py
@@ -53,4 +53,14 @@ class TestProductViewSet(APITestCase):
 
         self.assertEqual(created_product.title, 'notebook')
         self.assertEqual(created_product.price, 800.00)
+        
+    def test_delete_product(self):
+        response = self.client.delete(
+            reverse('product-detail', kwargs={'version': 'v1', 'pk': self.product.id})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        with self.assertRaises(Product.DoesNotExist):
+            Product.objects.get(id=self.product.id)
 
